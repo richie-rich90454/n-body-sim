cb3820fe0942a6ff90fc2463685411f6b60d97f8
richie-rich90454 <rjiang880@gmail.com>
2026-07-14 10:24:04 +0800

    add background star vertex shader

---
+++ src/visuals/shaders/background-star.vert.glsl
@@ -0,0 +1,9 @@
+attribute vec3 color;
+varying vec3 vColor;
+uniform float pointSize;
+void main() {
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+  gl_PointSize = max(1.5, pointSize * (300.0 / -mvPosition.z));
+  gl_Position = projectionMatrix * mvPosition;
+  vColor = color;
+}
